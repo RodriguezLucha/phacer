b4e262496f998d0095c9a46697b3d508f9b411a1
Rudy <RodriguezLucha@gmail.com>
2019-03-10 13:41:27 -0700

    Converted to Phaser 2, basic movement implemented

--- frontend/src/components/single_player.jsx
+++ frontend/src/components/single_player.jsx
@@ -1,54 +1,57 @@
 import React, { Component } from 'react'
-import Phaser from 'phaser';
+import Phaser from 'phaser-ce';
 
 export default class SinglePlayer extends Component {
   componentDidMount() {
 
-    var config = {
-      type: Phaser.AUTO,
-      width: 400,
-      height: 400,
-      parent: 'phaser-example',
-      physics: {
-        default: 'arcade',
-        arcade: {
-          debug: true,
-          gravity: { y: 100 }
-        }
-      },
-      scene: {
-        preload: this.preload,
-        create: this.create
-      }
-    };
-
-    this.game = new Phaser.Game(config);
-    console.log(this.game);
+    this.game = new Phaser.Game(400,400, Phaser.CANVAS, 'phaser-container', {
+      preload: this.preload,
+      create: this.create,
+      update: this.update
+    });
 
   }
 
-
   preload() {
-    this.load.image('ball', 'small.ico');
+    this.game.load.image('atari', 'game/atari130xe.png');
+    this.game.load.image('sky', 'game/sunset.png');
   }
 
   create() {
-    let ball1 = this.physics.add.image(100, 240, 'ball');
-    let ball2 = this.physics.add.image(100, 240, 'ball');
 
-    ball1.setCircle(46);
-    ball2.setCircle(46);
+    this.game.add.image(0,0, 'sky');
+    this.game.physics.startSystem(Phaser.Physics.P2JS);
 
-    ball1.setCollideWorldBounds(true);
-    ball2.setCollideWorldBounds(true);
+    //  Make things a bit more bouncey
+    this.game.physics.p2.defaultRestitution = 0.8;
 
-    ball1.setBounce(1);
-    ball2.setBounce(1);
+    //  Add a sprite
+    this.sprite = this.game.add.sprite(200, 200, 'atari');
+    this.game.physics.enable(this.sprite, Phaser.Physics.P2JS)
 
-    ball1.setVelocity(150);
-    ball2.setVelocity(-200, 60);
+    console.log(this.sprite);
+    this.sprite.body.setZeroDamping();
+    this.sprite.body.fixedRotation = true;
+    this.cursors = this.game.input.keyboard.createCursorKeys();
 
-    this.physics.add.collider(ball1, ball2);
+
+  }
+  update() {
+    this.sprite.body.setZeroVelocity();
+
+    if (this.cursors.left.isDown) {
+      this.sprite.body.moveLeft(400);
+    }
+    else if (this.cursors.right.isDown) {
+      this.sprite.body.moveRight(400);
+    }
+
+    if (this.cursors.up.isDown) {
+      this.sprite.body.moveUp(400);
+    }
+    else if (this.cursors.down.isDown) {
+      this.sprite.body.moveDown(400);
+    }
   }
 
 
