52122dd08066ec7d06de1f455f3b1394b0c9e6cb
Rudy <RodriguezLucha@gmail.com>
2019-03-11 11:08:53 -0700

    Add sockets to server

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const socket = require('socket.io');
 
-const users = require("./routes/api/users");
-const rooms = require("./routes/api/rooms");
+const users = require('./routes/api/users');
+const rooms = require('./routes/api/rooms');
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
+  .connect(db, {useNewUrlParser: true})
+  .then(() => console.log('Connected to MongoDB successfully'))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!!"));
+app.get('/', (req, res) => res.send('Hello World!!'));
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use("/api/users", users);
-app.use("/api/rooms", rooms);
+app.use('/api/users', users);
+app.use('/api/rooms', rooms);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+const server = app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log(socket.id);
+
+  socket.on('SEND_MESSAGE', (data) => {
+    io.emit('RECEIVE_MESSAGE', data);
+  });
+});
