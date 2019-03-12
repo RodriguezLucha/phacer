05ac74ae61c9f92d7c5fc5c68b70d9f284572cac
Rudy <RodriguezLucha@gmail.com>
2019-03-12 15:23:04 -0700

    Add fire

--- frontend/src/components/single_player/single_player.jsx
+++ frontend/src/components/single_player/single_player.jsx
@@ -23,10 +23,16 @@ export default class SinglePlayer extends Component {
     this.game.load.image('ground_1x1', 'game/ground_1x1.png');
     this.game.load.image('walls_1x2', 'game/walls_1x2.png');
     this.game.load.image('tiles2', 'game/tiles2.png');
+
+    this.game.load.image('fire1', 'game/fire1.png');
+    this.game.load.image('fire2', 'game/fire2.png');
+    this.game.load.image('fire3', 'game/fire3.png');
+    this.game.load.image('smoke', 'game/smoke-puff.png');
   }
 
   create() {
     this.game.physics.startSystem(Phaser.Physics.P2JS);
+    this.game.stage.disableVisibilityChange = true;
 
     this.map = this.game.add.tilemap('map');
 
@@ -37,6 +43,19 @@ export default class SinglePlayer extends Component {
     this.layer.resizeWorld();
 
     this.map.setCollisionBetween(1, 12);
+
+
+
+    this.emitter = this.game.add.emitter(this.game.world.centerX, this.game.world.centerY, 400);
+
+    this.emitter.makeParticles(['fire1', 'fire2', 'fire3']);
+
+    this.emitter.gravity = 0;
+    this.emitter.setAlpha(1, 0, 3000);
+    this.emitter.setScale(0.50, 0, 0.50, 0, 2000);
+
+    this.emitter.start(false, 3000, 5);
+
 
     this.game.physics.p2.convertTilemap(this.map, this.layer);
 
@@ -60,6 +79,20 @@ export default class SinglePlayer extends Component {
         ship.body.velocity.y = ship.body.velocity.y / 1.05;
       }
     }
+    if (cursors.down.isDown) { ship.body.thrust(-400); }
+
+    let px = ship.body.velocity.x;
+    let py = ship.body.velocity.y;
+
+    px *= -1;
+    py *= -1;
+
+    this.emitter.minParticleSpeed.set(px, py);
+    this.emitter.maxParticleSpeed.set(px, py);
+
+    this.emitter.emitX = ship.x;
+    this.emitter.emitY = ship.y;
+
   }
 
 shouldComponentUpdate(nextProps, nextState) {
